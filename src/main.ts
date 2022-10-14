dffff21c386e7f8a9b5b19eab4ab6a784f2f5a11
cz2542079957 <2542079957@qq.com>
2022-10-14 21:58:46 +0800

    清理

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,9 @@ import App from "./App.vue";
 import init from "./init";
 import router from "./router";
 import { createPinia } from "pinia";
-// import store from "./store/index";
 import mountServerFunctions from "./mountServerFunctions";
 
 const app = createApp(App);
-// app.use(store);
 app.use(createPinia());
 app.use(router);
 init(app);
