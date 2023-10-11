abf24e17138601a537806bd4f77141c9ba950608
JorbanSS <jorbansg@gmail.com>
2023-10-11 15:38:01 +0000

    管理 UI 更新

--- src/components/Admin/ProblemEditChildren/AddProblem.vue
+++ src/components/Admin/ProblemEditChildren/AddProblem.vue
@@ -1,63 +1,18 @@
 <template>
-    <!-- <el-form :model="form" label-width="120px">
-        <el-form-item label="Activity name">
-            <el-input v-model="form.name" />
-        </el-form-item>
-        <el-form-item label="Activity zone">
-            <el-select v-model="form.region" placeholder="please select your zone">
-                <el-option label="Zone one" value="shanghai" />
-                <el-option label="Zone two" value="beijing" />
-            </el-select>
-        </el-form-item>
-        <el-form-item label="Activity time">
-            <el-col :span="11">
-                <el-date-picker v-model="form.date1" type="date" placeholder="Pick a date" style="width: 100%" />
-            </el-col>
-            <el-col :span="2" class="text-center">
-                <span class="text-gray-500">-</span>
-            </el-col>
-            <el-col :span="11">
-                <el-time-picker v-model="form.date2" placeholder="Pick a time" style="width: 100%" />
-            </el-col>
-        </el-form-item>
-        <el-form-item label="Instant delivery">
-            <el-switch v-model="form.delivery" />
-        </el-form-item>
-        <el-form-item label="Activity type">
-            <el-checkbox-group v-model="form.type">
-                <el-checkbox label="Online activities" name="type" />
-                <el-checkbox label="Promotion activities" name="type" />
-                <el-checkbox label="Offline activities" name="type" />
-                <el-checkbox label="Simple brand exposure" name="type" />
-            </el-checkbox-group>
-        </el-form-item>
-        <el-form-item label="Resources">
-            <el-radio-group v-model="form.resource">
-                <el-radio label="Sponsor" />
-                <el-radio label="Venue" />
-            </el-radio-group>
-        </el-form-item>
-        <el-form-item label="Activity form">
-            <el-input v-model="form.desc" type="textarea" />
-        </el-form-item>
-        <el-form-item>
-            <el-button type="primary" @click="onSubmit">Create</el-button>
-            <el-button>Cancel</el-button>
-        </el-form-item>
-    </el-form> -->
     <div class="addProblem">
-        <div><span>标题</span>
+        <div>
+            <span>标题</span>
             <el-input v-model="problem.Title" />
         </div>
         <div>
             <span>时间限制</span>
-            <el-input-number v-model="problem.LimitTime" :min="1" />
+            <el-input-number v-model="problem.LimitTime" :min="100" :step="100" />
             <span>&nbsp;毫秒</span>
         </div>
         <div>
             <span>内存限制</span>
-            <el-input-number v-model="problem.LimitMemory" :min="1" />
-            <span>&nbsp;MB</span>
+            <el-input-number v-model="problem.LimitMemory" :min="64" :step="64" controls-position="right"/>
+            <span>&nbsp;MiB</span>
         </div>
         <div>
             <span>来源</span>
@@ -339,7 +294,7 @@ function complete() {
 
 <style scoped lang="scss">
 .addProblem {
-    // width: 100%;
+    width: 100%;
     display: flex;
     flex-direction: column;
 
